d568c3d81406d5ab1c1af5744689354344505996
unknown <caro.echevarria1991@gmail.com>
2021-07-02 16:46:03 -0500

    Updated Search.js

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function Weather() {
+  return (
+<form >
+      <input type="search" placeholder="Enter a city.."  />
+      <button type="Submit" id="search-button">
+        <i className="fa fa-search"></i>
+      </button>
+      <button type="submit" id="current-button">
+        <i className="fas fa-map-marker-alt"></i>
+      </button>
+    </form>)
+}
